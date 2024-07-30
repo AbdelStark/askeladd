7597667d3a522ddde3fb8c636d6c5da628357854
MSghais <msghais135@gmail.com>
2024-07-30 15:36:31 +0200

    env default relayer

--- askeladd-dvm-marketplace/src/constants/relay.ts
+++ askeladd-dvm-marketplace/src/constants/relay.ts
@@ -2,5 +2,5 @@ export const ASKELADD_RELAY= [
     // DVM service prover deployed need to subscribed to this relays
     // "ws://127.0.0.1:8080", // run in local
     // "wss://relay.nostr.net",
-    'wss://nostr-relay-nestjs-production.up.railway.app', // AFK relayer
+    process.env.NEXT_PUBLIC_DEFAULT_RELAYER ?? 'wss://nostr-relay-nestjs-production.up.railway.app',// AFK relayer
 ]
