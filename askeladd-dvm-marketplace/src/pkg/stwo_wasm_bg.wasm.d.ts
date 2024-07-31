eb333469439eeb0cc19c75f76777ea965cd9372e
A₿del ∞/21M 🐺 - 🐱 <45264458+AbdelStark@users.noreply.github.com>
2024-07-31 18:57:10 +0200

    Debug/proof verification (#18)
    * debug
    
    * debug

--- askeladd-dvm-marketplace/src/pkg/stwo_wasm_bg.wasm.d.ts
+++ askeladd-dvm-marketplace/src/pkg/stwo_wasm_bg.wasm.d.ts
@@ -1,11 +1,11 @@
 /* tslint:disable */
 /* eslint-disable */
 export const memory: WebAssembly.Memory;
-export function __wbg_fibonacciresult_free(a: number): void;
-export function fibonacciresult_success(a: number): number;
-export function fibonacciresult_message(a: number, b: number): void;
-export function run_fibonacci_example(a: number, b: number): number;
-export function run_fibonacci_verify_exemple(a: number, b: number, c: number, d: number): number;
+export function __wbg_stworesult_free(a: number): void;
+export function stworesult_success(a: number): number;
+export function stworesult_message(a: number, b: number): void;
+export function prove_and_verify(a: number, b: number): number;
+export function verify_stark_proof(a: number, b: number, c: number, d: number): number;
 export function __wbindgen_add_to_stack_pointer(a: number): number;
 export function __wbindgen_free(a: number, b: number, c: number): void;
 export function __wbindgen_malloc(a: number, b: number): number;
---
+++ crates/stwo_wasm/src/lib.rs
@@ -0,0 +1,125 @@
+// lib.rs
+
+use serde::{Deserialize, Serialize};
+use stwo_prover::core::fields::m31::BaseField;
+use stwo_prover::core::prover::StarkProof;
+use stwo_prover::examples::fibonacci::Fibonacci;
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern "C" {
+    // Use `js_namespace` here to bind `console.log(..)` instead of just
+    // `log(..)`
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+}
+
+macro_rules! console_log {
+    // Note that this is using the `log` function imported above
+    ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
+}
+
+#[derive(Serialize, Deserialize)]
+#[wasm_bindgen]
+pub struct StwoResult {
+    success: bool,
+    message: String,
+}
+
+#[wasm_bindgen]
+impl StwoResult {
+    #[wasm_bindgen(getter)]
+    pub fn success(&self) -> bool {
+        self.success
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn message(&self) -> String {
+        self.message.clone()
+    }
+}
+
+#[wasm_bindgen]
+pub fn prove_and_verify(log_size: u32, claim: u32) -> StwoResult {
+    console_log!(
+        "Starting prove_and_verify with log_size: {}, claim: {}",
+        log_size,
+        claim
+    );
+    let fib = Fibonacci::new(log_size, BaseField::from(claim));
+
+    match fib.prove() {
+        Ok(proof) => {
+            console_log!("Proof generated successfully");
+            let serialized = serde_json::to_string(&proof).unwrap();
+            console_log!("Serialized proof: {}", serialized);
+
+            match fib.verify(proof) {
+                Ok(_) => {
+                    console_log!("Proof verified successfully");
+                    StwoResult {
+                        success: true,
+                        message: serialized.to_string(),
+                    }
+                }
+                Err(e) => {
+                    console_log!("Proof verification failed: {:?}", e);
+                    StwoResult {
+                        success: false,
+                        message: format!("Proof verification failed: {:?}", e),
+                    }
+                }
+            }
+        }
+        Err(e) => {
+            console_log!("Proof generation failed: {:?}", e);
+            StwoResult {
+                success: false,
+                message: format!("Proof generation failed: {:?}", e),
+            }
+        }
+    }
+}
+
+#[wasm_bindgen]
+pub fn verify_stark_proof(log_size: u32, claim: u32, stark_proof_str: &str) -> StwoResult {
+    console_log!(
+        "Starting verify_stark_proof with log_size: {}, claim: {}",
+        log_size,
+        claim
+    );
+    console_log!("Received proof string length: {}", stark_proof_str.len());
+
+    let fib = Fibonacci::new(log_size, BaseField::from(claim));
+
+    let stark_proof: Result<StarkProof, serde_json::Error> = serde_json::from_str(stark_proof_str);
+
+    match stark_proof {
+        Ok(proof) => {
+            console_log!("Proof deserialized successfully");
+            match fib.verify(proof) {
+                Ok(()) => {
+                    console_log!("Proof verified successfully");
+                    StwoResult {
+                        success: true,
+                        message: "Proof verified successfully".to_string(),
+                    }
+                }
+                Err(e) => {
+                    console_log!("Proof verification failed: {:?}", e);
+                    StwoResult {
+                        success: false,
+                        message: format!("Proof verification failed: {:?}", e),
+                    }
+                }
+            }
+        }
+        Err(e) => {
+            console_log!("Failed to deserialize proof: {:?}", e);
+            StwoResult {
+                success: false,
+                message: format!("Failed to deserialize proof: {:?}", e),
+            }
+        }
+    }
+}
